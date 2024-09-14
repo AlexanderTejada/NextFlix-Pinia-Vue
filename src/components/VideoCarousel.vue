125a2cdd51c523d9d0334aa63127079013a79889
AlexanderTejada <120703301+AlexanderTejada@users.noreply.github.com>
2024-09-14 19:43:49 -0300

    images and videos

--- src/components/VideoCarousel.vue
+++ src/components/VideoCarousel.vue
@@ -20,8 +20,21 @@
     v-for="slide, index in movies"
     :key="slide"
     class="flex items-center object-cover text-white bg-transparent">
-      {{ slide }}
-       
+<div
+  @click="event => fullScreeVideo(index)"
+  class="object-cover h-[100%] hover:brightness-125 cursor-pointer"
+  :class="
+  currentSlide !== index? 'border-4 border-transparent' : 'border-4 border-white',
+  currentSlideObject(slide, index)
+  "
+
+    >
+    <img 
+       style="user-select: none"
+       class="pointer-events-none h- [100%] z-[1]"
+       :src="'/images/'+slide.name+'.png'" >
+  
+    </div>       
   </Slide>
 
        </Carousel>
